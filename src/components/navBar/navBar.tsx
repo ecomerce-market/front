3ac7c663b86579c9364e8841cdfc047d3a7aa0dd
ggaong <gain223@naver.com>
2025-02-03 20:33:39 +0900

    [Feat] navBar / category 연결

--- src/components/navBar/navBar.tsx
+++ src/components/navBar/navBar.tsx
@@ -1,18 +1,66 @@
-import React from "react";
+"use client";
+
+import React, { useState, useEffect, useRef } from "react";
 import styles from "./navBar.module.scss";
 import cn from "classnames/bind";
 import { TbMenu2 } from "react-icons/tb";
+import Category from "../category/category";
 
 const cx = cn.bind(styles);
 
-const navBar = () => {
+const NavBar = () => {
+    const [isCategoryOpen, setIsCategoryOpen] = useState(false);
+    const [isCategoryActive, setIsCategoryActive] = useState(false);
+    const categoryRef = useRef<HTMLDivElement | null>(null);
+
+    // 카테고리 컴포넌트 외부 클릭 시 카테고리 닫히게 처리
+    useEffect(() => {
+        const handleClickOutside = (event: MouseEvent) => {
+            if (
+                categoryRef.current &&
+                !categoryRef.current.contains(event.target as Node)
+            ) {
+                setIsCategoryOpen(false);
+                setIsCategoryActive(false);
+            }
+        };
+
+        document.addEventListener("click", handleClickOutside);
+
+        return () => {
+            document.removeEventListener("click", handleClickOutside);
+        };
+    }, []);
+
+    const toggleCategory = () => {
+        setIsCategoryOpen((prev) => !prev);
+        setIsCategoryActive((prev) => !prev);
+    };
+
     return (
         <div className={cx("navBarWrapper")}>
-            <div className={cx("category")}>
-                <div className={cx("categoryIcon")}>
+            <div className={cx("category")} ref={categoryRef}>
+                <div
+                    className={cx("categoryIcon", {
+                        active: isCategoryActive,
+                    })}
+                    onClick={toggleCategory}
+                >
                     <TbMenu2 />
                 </div>
-                <div className={cx("categorytitle")}>카테고리</div>
+                <div
+                    className={cx("categoryTitle", {
+                        active: isCategoryActive,
+                    })}
+                    onClick={toggleCategory}
+                >
+                    카테고리
+                </div>
+                {isCategoryOpen && (
+                    <div className={cx("categoryContent")}>
+                        <Category />
+                    </div>
+                )}
             </div>
             <div className={cx("subCategory")}>
                 <p>신상품</p>
@@ -24,4 +72,4 @@ const navBar = () => {
     );
 };
 
-export default navBar;
+export default NavBar;
