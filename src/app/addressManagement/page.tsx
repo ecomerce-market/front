71060192244830c68b510cc6aa9b6c2551a3fe79
ggaong <gain223@naver.com>
2025-02-04 20:03:15 +0900

    [Feat] 배송지 관리 화면 작업

---
+++ src/app/addressManagement/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AddressManagement from "./addressManagement";
+
+const page = () => {
+    return (
+        <div>
+            <AddressManagement />
+        </div>
+    );
+};
+
+export default page;
