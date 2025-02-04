a4283d6439b18679cde861817dce78f9f9f2b08d
ggaong <gain223@naver.com>
2025-02-04 16:24:40 +0900

    [Feat] 개인정보수정(정보 수정) 화면 작업

---
+++ src/app/myInfoDetail/myInfoDetail.tsx
@@ -0,0 +1,82 @@
+import React from "react";
+import styles from "./myInfoDetail.module.scss";
+import cn from "classnames/bind";
+import TextInput from "@/components/input/textInput";
+import PersonalInfo from "@/components/personalnfo/personalInfo";
+import SideMenu from "@/components/sideMenu/sideMenu";
+import TwoBtn from "@/components/btn/twoBtn";
+
+const cx = cn.bind(styles);
+
+const myInfoDetail = () => {
+    return (
+        <div className={cx("myInfoDetailWrapper")}>
+            <PersonalInfo
+                grade={"일반"}
+                username={"김철수"}
+                credit={"3,000"}
+                expiringCredit={"50"}
+                coupon={"4"}
+            />
+            <div className={cx("myInfoMain")}>
+                <div className={cx("sideMenu")}>
+                    <SideMenu
+                        title={"마이컬리"}
+                        content={[
+                            "주문내역",
+                            "찜한상품",
+                            "배송지 관리",
+                            "상품 후기",
+                            "결제수단",
+                        ]}
+                    />
+                </div>
+                <div className={cx("checkInfo")}>
+                    <h1 className={cx("mainTitle")}>개인 정보 수정</h1>
+                    <div className={cx("submitForm")}>
+                        <p>
+                            <p>아이디</p>
+                            <TextInput />
+                        </p>
+                        <p>
+                            <p>현재 비밀번호</p>
+                            <TextInput />
+                        </p>
+                        <p>
+                            <p>새 비밀번호</p>
+                            <TextInput />
+                        </p>
+                        <p>
+                            <p>새 비밀번호 확인</p>
+                            <TextInput />
+                        </p>
+                        <p>
+                            <p>이름</p>
+                            <TextInput />
+                        </p>
+                        <p>
+                            <p>이메일</p>
+                            <TextInput />
+                        </p>
+                        <p>
+                            <p>휻대폰</p>
+                            <TextInput />
+                        </p>
+                        <p>
+                            <p>생년월일</p>
+                            <TextInput />
+                        </p>
+                    </div>
+                    <div className={cx("submitBtn")}>
+                        <TwoBtn
+                            leftTitle={"탈퇴하기"}
+                            rightTitle={"회원정보 수정"}
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default myInfoDetail;
