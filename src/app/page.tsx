442dbaa1a95d5324db3d58e178fa088b3b5a2337
taeyoung <her020212@naver.com>
2025-02-04 18:03:09 +0900

    [Feat]로그인 화면 시작

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import CartCard from "@/components/productCard/cart/cartCard";
 export default function Home() {
   return (
     <div>
-      <h1>HEY GAYOUNG WE CAN WIN</h1>
+      <h1>로그인</h1>
     </div>
   );
 }
