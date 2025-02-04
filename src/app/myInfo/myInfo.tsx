bc5e20bb9e378d2fe2515edb041eb48faa33d303
ggaong <gain223@naver.com>
2025-02-04 15:57:34 +0900

    [Feat] 개인정보수정(비밀번호 재확인)  화면 작업

---
+++ src/app/myInfo/myInfo.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+import styles from "./myInfo.module.scss";
+import cn from "classnames/bind";
+import PersonalInfo from "@/components/personalnfo/personalInfo";
+import SideMenu from "@/components/sideMenu/sideMenu";
+import TextInput from "@/components/input/textInput";
+import OneBtn from "@/components/btn/oneBtn";
+
+const cx = cn.bind(styles);
+
+const myInfo = () => {
+    return (
+        <div className={cx("myInfoWrapper")}>
+            <PersonalInfo
+                grade={"일반"}
+                username={"김철수"}
+                credit={"3,000"}
+                expiringCredit={"50"}
+                coupon={"4"}
+            />
+            <div className={cx("myInfoMain")}>
+                <div className={cx("sideMenu")}>
+                    <SideMenu
+                        title={"마이컬리"}
+                        content={[
+                            "주문내역",
+                            "찜한상품",
+                            "배송지 관리",
+                            "상품 후기",
+                            "결제수단",
+                        ]}
+                    />
+                </div>
+                <div className={cx("checkInfo")}>
+                    <h1 className={cx("mainTitle")}>개인 정보 수정</h1>
+                    <div>
+                        <h2 className={cx("subTitle")}>비밀번호 재확인</h2>
+                        <p className={cx("content")}>
+                            회원님의 정보를 안전하게 보호하기 위해 비밀번호를
+                            다시 한번 확인해주세요.
+                        </p>
+                    </div>
+                    <div className={cx("submitForm")}>
+                        <div className={cx("submitInput")}>
+                            <p>아이디</p>
+                            <TextInput />
+                        </div>
+                        <div className={cx("submitInput")}>
+                            <div className={cx("password")}>
+                                <p>비밀번호</p>
+                                <p className={cx("require")}>*</p>
+                            </div>
+                            <TextInput />
+                        </div>
+                    </div>
+                    <div className={cx("submitBtn")}>
+                        <OneBtn
+                            title={"확인"}
+                            width={"320"}
+                            bgcolor={"--main-color"}
+                            color={"--white"}
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default myInfo;
