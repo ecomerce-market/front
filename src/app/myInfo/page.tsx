d363e40f3670c9535a10a524bd24c8344d8553a1
ggaong <gain223@naver.com>
2025-02-04 15:58:36 +0900

    [Feat] 개인정보수정(비밀번호 재확인) 화면 작업

---
+++ src/app/myInfo/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MyInfo from "./myInfo";
+
+const page = () => {
+    return (
+        <div>
+            <MyInfo />
+        </div>
+    );
+};
+
+export default page;
