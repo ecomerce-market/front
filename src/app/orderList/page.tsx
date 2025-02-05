44a2a98ad1367d7e0a77b71a9f566110d93d844c
ggaong <gain223@naver.com>
2025-02-05 22:11:31 +0900

    [Feat] 주문 내역 상세 화면 작업

---
+++ src/app/orderList/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import OrderList from "./orderList";
+
+const page = () => {
+    return (
+        <div>
+            <OrderList />
+        </div>
+    );
+};
+
+export default page;
+<OrderList />;
