7c54b2d49ea87235d24c665a9dee8ae36eea32de
ggaong <gain223@naver.com>
2025-02-05 23:59:55 +0900

    [Feat] 1:1 문의 화면 작업중

---
+++ src/app/inquiry/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Inquiry from "./inquiry";
+
+const page = () => {
+    return (
+        <div>
+            <Inquiry />
+        </div>
+    );
+};
+
+export default page;
