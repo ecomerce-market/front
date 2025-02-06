acda3d6eab7590efb9b2b825f75608571793d5e8
taeyoung <her020212@naver.com>
2025-02-06 12:45:08 +0900

    [Feat] 404페이지 생성

---
+++ src/app/findId/findId.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./findId.module.scss";
+import cn from "classnames/bind";
+const cx = cn.bind(styles);
+
+const FindId = () => {
+  return <div></div>;
+};
+
+export default FindId;
