54810e3e8a1e275f4eb2f9ab7a53bf7c6c2b0157
taeyoung <her020212@naver.com>
2025-02-24 19:52:40 +0900

    [Design] 네브바 반응형 수정

--- src/app/main/main.tsx
+++ src/app/main/main.tsx
@@ -1,17 +1,34 @@
-import Product from "@/components/main/product/product";
+import dynamic from "next/dynamic";
 import styles from "./main.module.scss";
 import cn from "classnames/bind";
 import React from "react";
-import WeekendProduct from "@/components/main/weekend/weekendProduct";
-import NewProduct from "@/components/main/newProduct/newProduct";
 import BannerSwiper from "@/components/main/bannerSwiper/bannerSwiper";
+
 const cx = cn.bind(styles);
+
+const Product = dynamic(() => import("@/components/main/product/product"), {
+  loading: () => <p>상품 불러오는 중...</p>,
+});
+const WeekendProduct = dynamic(
+  () => import("@/components/main/weekend/weekendProduct"),
+  {
+    loading: () => <p>주말 특가 불러오는 중...</p>,
+  }
+);
+const NewProduct = dynamic(
+  () => import("@/components/main/newProduct/newProduct"),
+  {
+    loading: () => <p>신상품 불러오는 중...</p>,
+  }
+);
 
 const Main = () => {
   return (
     <div className={cx("main-wrapper")}>
       <BannerSwiper />
+
       <h2 style={{ marginTop: "20px", marginBottom: "20px" }}>마감세일</h2>
+
       <Product />
       <WeekendProduct />
       <NewProduct />
